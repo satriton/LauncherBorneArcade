5cea18b042ce93e2176d9911ed0d53c7918340eb
satriton <80455175+satriton@users.noreply.github.com>
2022-03-14 22:28:38 +0100

    Dynamic load of games
    Games/ is for tests

--- LauncherArcade/Assets/Scripts/SC_BtnStartGame.cs
+++ LauncherArcade/Assets/Scripts/SC_BtnStartGame.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.UI;
 
 public class SC_BtnStartGame : MonoBehaviour
 {
-    public string PathToExe;
+    // public string PathToExe;
+    // public Image imageComponent;
 
     private SC_Launcher _scLauncher;
 
     void Start()
     {
-
+        // imageComponent = GetComponent<Image>();
     }
 
     void Update()
--- LauncherArcade/Assets/Scripts/SC_Launcher.cs
+++ LauncherArcade/Assets/Scripts/SC_Launcher.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SC_Launcher : MonoBehaviour
 {
@@ -10,50 +14,58 @@ public class SC_Launcher : MonoBehaviour
     // Charge toutes les données qu'il a récupérer dans l'UI et les bouttons
 
     public GameObject gameBtnPrefab;
-    [Range(1, 5)] public int nbGameByPage;
+    [Range(1, 4)] public int nbGameByPage;
     public static string pathToGames;
-    //public TMP_Text
-
-
+    public static string pathToGameMetaDefault;
+    public TextMeshProUGUI newsZone;
 
 
     private DirectoryInfo dirInfo;
     private Process process;
 
 
-
     private struct Game
     {
         public string pathToGameDir { get; set; }
         public string pathToExe { get; set; }
-        public string pathToAsset { get; set; }
+        public string pathToGameMeta { get; set; }
         public string name;
     }
     private Game[] games;
 
     private GameObject[] gamesBtn;
+    private int currentGameIndex;
 
 
 
     void Start()
     {
-        pathToGames = Application.dataPath + "/../../Games/";
+        pathToGames = Application.dataPath + "/../Games/";
+        pathToGameMetaDefault = pathToGames + "../GameMetaDefault";
+        currentGameIndex = 0;
 
 
         // Instanciate gamesBtn
         gamesBtn = new GameObject[nbGameByPage];
         for (int i = 0; i < nbGameByPage; i++)
         {
-            var gameBtn = gamesBtn[i];
+            GameObject gameBtn;
             gameBtn = Instantiate(gameBtnPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            gameBtn.transform.parent = transform;
+            gameBtn.transform.SetParent(transform);
             gameBtn.GetComponent<RectTransform>().localScale = Vector3.one;
+            // Image oui = gameBtn.GetComponent<Image>();
+            // .image = Resources.Load<Texture2D>("OverFoyIcon");
+
+            gamesBtn[i] = gameBtn;
         }
 
+        newsZone.text = pathToGameMetaDefault;
 
         updateGameList();
 
+        loadGameBtnAssets(0);
 
+        loadNews();
 
     }
 
@@ -65,21 +77,92 @@ public class SC_Launcher : MonoBehaviour
         // }
     }
 
+
     public void LaunchchGame(string pathToExe)
     {
         process = Process.Start(pathToExe);
     }
+
 
     private void changePage(bool boolGoRight)
     {
 
     }
 
+
+    private void loadNews()
+    {
+        var pathNewsTxt = pathToGames + "news.txt";
+        var strBuilder = new StringBuilder();
+
+
+        if (File.Exists(pathNewsTxt))
+        {
+            StreamReader reader = new StreamReader(pathNewsTxt);
+
+            while (!reader.EndOfStream)
+            {
+                strBuilder.Append(reader.ReadLine() + "\n");
+            }
+
+            reader.Close();
+        }
+        else
+        {
+            strBuilder.Append("Pas de news");
+        }
+
+        newsZone.text = strBuilder.ToString();
+    }
+
+
     // Load assets (and description ?) on the gamesBtn for games[index] à games[index + nbGameByPage]
     private void loadGameBtnAssets(int index)
     {
+        currentGameIndex = index;
 
+        for (int i = 0; i < nbGameByPage; i++)
+        {
+            var gameBtnImage = gamesBtn[i].GetComponent<Image>();
+            float PixelsPerUnit = 100.0f;
+            string pathToGameLogo = games[i + index].pathToGameMeta + "/logo.png";
+
+            Texture2D SpriteTexture;
+            Sprite gameImage;
+            if (File.Exists(pathToGameLogo))
+            {
+                SpriteTexture = LoadTexture(pathToGameLogo);
+                gameImage = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit);
+            }
+            else
+            {
+                SpriteTexture = LoadTexture(pathToGameMetaDefault + "/logo.png");
+                gameImage = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit);
+            }
+
+            gameBtnImage.sprite = gameImage;
+            gameBtnImage.color = Color.white;
+        }
     }
+
+
+    // Load a PNG or JPG file from disk to a Texture2D
+    // Returns null if load fails
+    public Texture2D LoadTexture(string filePath)
+    {
+        Texture2D tex2D;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+            if (tex2D.LoadImage(fileData))           // Load the imagedata into the texture (size is set automatically)
+                return tex2D;                 // If data = readable -> return texture
+        }
+        return null;
+    }
+
 
     // Get all games in Games/ directory and fill games with all the info
     private void updateGameList()
@@ -95,15 +178,36 @@ public class SC_Launcher : MonoBehaviour
             games[j].pathToGameDir = pathToGame;
 
             var files = System.IO.Directory.GetFiles(pathToGame, "*.exe");
-            games[j].pathToExe = files[0]; // Il faudrait vérifier que ce soit bien le bon .exe par pas celui de l'handler ---------------------------------------------------
+
+            // On regarde que ce ne soit pas l'handlerUnity
+            // if (files[0].Contains("UnityCrashHandler"))
+            // {
+            //     games[j].pathToExe = files[1];
+            //     games[j].name = files[1].Split('.')[0];
+            // }
+            // else
+            // {
+            //     games[j].pathToExe = files[0];
+            //     games[j].name = files[0].Split('.')[0];
+            // }
+
+            games[j].pathToExe = files[0];
             games[j].name = files[0].Split('.')[0];
 
             var dir = System.IO.Directory.GetDirectories(pathToGame, "GameMeta");
-            games[j].pathToAsset = dir[0]; // Il faudrait vérifier que le dossier existe et mettre un chemin pour ---------------------------------------------------
+            //On vérifie que le dossier existe sinon on prend le dossier GameMetaDefault
+            if (dir == null)
+            {
+                games[j].pathToGameMeta = pathToGames + "../GameMetaDefault";
+            }
+            else
+            {
+                games[j].pathToGameMeta = dir[0];
+            }
 
             // UnityEngine.Debug.Log("gamedir: " + games[j].pathToGameDir + "\n");
             // UnityEngine.Debug.Log("pathexe: " + games[j].pathToExe + "\n");
-            // UnityEngine.Debug.Log("GameMeta: " + games[j].pathToAsset + "\n");
+            // UnityEngine.Debug.Log("GameMeta: " + games[j].pathToGameMeta + "\n");
             // UnityEngine.Debug.Log("name: " + games[j].name + "\n\n");
             j++;
         }
